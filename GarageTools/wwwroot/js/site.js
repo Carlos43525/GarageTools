5a750a766309535a3bd511f3ca3ab0905dcbff87
Carlos Rodriquez <carlos43525@gmail.com>
2022-05-08 04:46:54 -0400

    Finished homepage carousel and added placeholder images to wwwroot for development

--- GarageTools/wwwroot/js/site.js
+++ GarageTools/wwwroot/js/site.js
@@ -2,6 +2,7 @@
 const sideBarOpen = document.getElementById('sideBarOpen');
 const sideBar = document.getElementById('sideBar');
 
+// Mobile nav/sidebar open close handlers
 sideBarOpen.addEventListener('click', () => {
     sideBar.classList.toggle('-translate-x-full');
     sideBar.classList.add('translate-x-0');
@@ -12,17 +13,12 @@ sideBarClose.addEventListener('click', () => {
     sideBar.classList.toggle('-translate-x-full');
 })
 
-
-//document.addEventListener('DOMContentLoaded', function () {
-//    var splide = new Splide('.splide', {
-//        classes: {
-//            arrows: 'splide__arrows',
-//            arrow: 'splide__arrow',
-//            prev: 'splide__arrow--prev',
-//            next: 'splide__arrow--next',
-//        },
-//        type: 'loop',
-
-//    });
-//    splide.mount();
-//});
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#image-carousel', {
+        type: 'loop',
+        autoplay: true,
+        interval: 10000,
+        speed: 400,
+        pagination: false,
+    }).mount();
+});
