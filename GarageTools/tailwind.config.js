4b03dc342ee51f20f23ec6127a18d5c7db897e28
Carlos Rodriquez <carlos43525@gmail.com>
2022-05-14 04:00:50 -0400

    Added new color theme for site in tailwind config and continued to style the homepage

--- GarageTools/tailwind.config.js
+++ GarageTools/tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
         './Pages/**/*.cshtml'
     ],
     theme: {
+        extend: {
+            colors: {
+                'site-primary': '#2446A3',
+                'site-secondary': '#A38124', 
+                'site-tertiary': '#DEBE69',
+                'site-gray': '#1a1a1a'
+            }
+        }, 
         fontFamily: {
             sans: ['Roboto', 'sans-serif']
         }
